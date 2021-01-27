3e037dc8b6eae5d0799a0b58a6b0aadf3a73404e
Katherine <47138336+rocka840@users.noreply.github.com>
2021-01-27 11:40:58 +0100

    Add files via upload

---
+++ explode.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<form method="get" action="">
+    <input type="submit" name="submit">
+</form>
+
+
+<?php
+if(isset($_GET["submit"])){
+    $file = fopen("data9.txt", "r");
+    while($line = fgets($file)){
+        $exp = explode($line, "|");
+    }
+    print $exp;
+}
+?>
+
+</body>
+</html>
